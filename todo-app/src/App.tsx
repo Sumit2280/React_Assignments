0ca7d9ced8a7bd00d80bce9d785aa05b052698b2
Sumit Done <sumitdone123@gmail.com>
2024-03-06 11:09:42 +0530

    Formatted with prettier

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import ToDoList from './Components/ToDoList';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import ToDoList from "./Components/ToDoList";
 
 function App() {
   return (
     <>
-      <ToDoList/>
+      <ToDoList />
     </>
   );
 }
--- todo-app/src/Components/ToDoList.tsx
+++ todo-app/src/Components/ToDoList.tsx
@@ -1,106 +1,101 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import ToDo from "./ToDo";
 import IToDo from "../interfaces/Todo";
 import useFetch from "../hooks/useFetch";
 
-
 const ToDoList = () => {
-  const [todo, setToDo] = useState('');
+  const [todo, setToDo] = useState("");
   const [todolist, setToDoList] = useState<IToDo[]>([]);
-  const url = "http://localhost:8000/todo"
-  const { response, error, loader } = useFetch(url)
+  const url = "http://localhost:8000/todo";
+  const { response, error, loader } = useFetch(url);
 
   useEffect(() => {
     if (!error) {
-      setToDoList(response)
+      setToDoList(response);
+    } else {
+      alert(error);
     }
-    else {
-      alert(error)
-    }
-  }, [response, error])
+  }, [response, error]);
 
   const handleInput = (event: any) => {
-    setToDo(event.target.value)
-  }
+    setToDo(event.target.value);
+  };
 
   const saveInput = () => {
     if (todo) {
       const curr = {
         text: todo,
-        isCompleted: false
-      }
-      setToDoList([...todolist, curr])
-      setToDo('')
-    }
-    else {
+        isCompleted: false,
+      };
+      setToDoList([...todolist, curr]);
+      setToDo("");
+    } else {
       alert("enter the todo you want to add !");
     }
-  }
+  };
 
   const deleteToDo = (index: any) => {
     const updatedTodos = [...todolist];
     updatedTodos.splice(index, 1);
     setToDoList(updatedTodos);
-  }
+  };
 
   const handleCheck = (index: any) => {
     const currentTodo = todolist[index];
     currentTodo.isCompleted = !currentTodo.isCompleted;
-    setToDoList([...todolist])
-  }
-
-
+    setToDoList([...todolist]);
+  };
 
   return (
     <>
       <div>
         <input type="text" value={todo} onChange={handleInput} />
-        <button type="button" onClick={saveInput}>Add</button>
-        {loader ? (<h3>loading....</h3>) : (
+        <button type="button" onClick={saveInput}>
+          Add
+        </button>
+        {loader ? (
+          <h3>loading....</h3>
+        ) : (
           <div>
             <div>
               <h1>Not Completed Todo</h1>
-              {
-                todolist.map((item, index) => {
-                  if (!item.isCompleted) {
-                    return (
-                      <ul>
-                        <ToDo
-                          handleCheck={handleCheck}
-                          deleteToDo={deleteToDo}
-                          item={item}
-                          index={index}
-                        />
-                      </ul>
-                    )
-                  }
-                })
-              }
+              {todolist.map((item, index) => {
+                if (!item.isCompleted) {
+                  return (
+                    <ul>
+                      <ToDo
+                        handleCheck={handleCheck}
+                        deleteToDo={deleteToDo}
+                        item={item}
+                        index={index}
+                      />
+                    </ul>
+                  );
+                }
+              })}
             </div>
             <div>
               <h1>Completed Todo</h1>
-              {
-                todolist.map((item, index) => {
-                  if (item.isCompleted) {
-                    return (
-                      <ul>
-                        <ToDo
-                          handleCheck={handleCheck}
-                          deleteToDo={deleteToDo}
-                          item={item}
-                          index={index}
-                        />
-                      </ul>
-                    )
-                  }
-                })
-              }
+              {todolist.map((item, index) => {
+                if (item.isCompleted) {
+                  return (
+                    <ul>
+                      <ToDo
+                        handleCheck={handleCheck}
+                        deleteToDo={deleteToDo}
+                        item={item}
+                        index={index}
+                      />
+                    </ul>
+                  );
+                }
+              })}
             </div>
           </div>
         )}
       </div>
     </>
-  )
-}
+  );
+};
 
-export default ToDoList
+export default ToDoList;
